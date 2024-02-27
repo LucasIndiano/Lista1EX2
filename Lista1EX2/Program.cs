8e8af360042b22aa9820fe62007efe18a0cbcadf
ALUNO <ALUNO@LABHPC22>
2024-02-27 15:59:56 -0300

    Adicionar arquivos de projeto.

---
+++ Lista1EX2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista1EX2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            Console.WriteLine("Digite um Número: ");
+            n = int.Parse(Console.ReadLine());
+            if (n >= 20 && n <= 100) { Console.WriteLine("O Número " + n + " está entre 20 e 100."); }
+            else {Console.WriteLine("O Número " + n + " não está entre 20 e 100."); }
+            Console.ReadKey();
+        }
+    }
+}
